eccefbc5cf4f47900cc600252d588cd902a46abe
Anna Ikonomova <ani.strandjeva@gmail.com>
2019-03-19 16:19:22 +0200

    Fix Home component's divs

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,17 @@ import logo from './logo.png';
 class Home extends Component {
   render() {
     return (
-      <div className="boxheading">
-      <p className='hello'>Hey, I'm glad to see you here. :)</p>
+      <div className="boxhome">
+      <div className='hello'>
+      <p className='helloText'>Hey, I'm glad to see you here. :)</p>
+      </div>
+      <div className='boxheading'>
           <img src={logo} className="App-logo" alt="logo" />
           <header className="name">
           <h1> Anna Ikonomova </h1>
           <h2>Front-End Web developer</h2>
         </header>
+        </div>
         </div>
     );
   }
