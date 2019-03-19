082a1a520c22478153e8e11f85e2f69bab3471eb
Anna Ikonomova <ani.strandjeva@gmail.com>
2019-03-19 15:16:22 +0200

    Change text in About component

--- src/About.js
+++ src/About.js
@@ -7,12 +7,17 @@ class About extends Component {
     return (
       <div className="info">
       <img className='anna' src={anIko} alt='Anna Ikonomova'></img>
-      <p className='aboutMe'>My Name is Anna Ikonomova - a Front-end Developer.<br></br>
-      I love to create, to experiment and I'm a lifelong learner.<br></br>
+      <h3>About me</h3>
+      <p className='aboutMe'>My Name is Anna Ikonomova - Front End Developer.<br></br>
+      I love to create, to experiment and I love to learn.<br></br>
       There is nothing impossible!<br></br>
-  The first line of code, that I wrote, was when the Google Udacity Scolarship challenge <br></br>
-  course began. Now I'm still learning and definately willing to become a real professional.
+      The first line of code, that I wrote, was when the Google Udacity Scolarship challenge <br></br>
+      course began. Now I'm still learning and definately willing to become a real professional.
       </p>
+      <h3>Skills</h3>
+      <p> Html5, CSS3, JavaScript, React, Git, Git hub, Responsive Design and more.</p>
+      <h4>Soft skills</h4>
+      <p>Flexibility, Strong work ethic and team spirit.</p>
     </div>
 
     );
